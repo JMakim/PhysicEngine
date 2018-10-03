a511441cfe69f8dc89773ff90cfbaccad2fcb8da
Jahn Makim Ramjahn <jmakimramjahn@gmail.com>
2018-10-03 14:14:54 -0400

    new

---
+++ Lab1+2/Lab1+2/Assets/_Scripts/Game.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+ 
+    public Transform targetTransform;
+
+    private Rigidbody m_rb;
+    public float pushForce = 0.0f;
+    private bool m_isRunning = false;
+    private bool powerStroke = false;
+    public float power = 0.0f;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            powerStroke = true;
+            m_isRunning = true;
+
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            power = pushForce;
+            powerStroke = false;
+            m_isRunning = true;
+        }
+    }
+
+
+
+    void FixedUpdate()
+    {
+        if (powerStroke)
+            power = power + Time.time;
+        if (!powerStroke)
+            pushForce = power;
+        if (m_isRunning)
+            m_rb.AddForce(transform.forward * power);
+    }
+
+
+}
