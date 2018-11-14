e343f017e95f737ffb639583f2195ab043c7951a
Makim <jmakimramjahn@gmail.com>
2018-11-14 18:26:40 -0500

    init start

---
+++ PhysEngLab3-4/Assets/_Script/direction.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class direction : MonoBehaviour {
+
+    public Rigidbody Rb;
+
+    public GameObject xAxis;
+    public GameObject yAxis;
+
+    public bool dirLock = false;
+
+    public float power;
+
+	// Use this for initialization
+	void Start () {
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Aim();
+        Kick();
+	}
+
+    void Aim()
+    {
+        if (!dirLock)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                xAxis.transform.Rotate(Vector3.up * -10 * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                xAxis.transform.Rotate(Vector3.up * 10 * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                yAxis.transform.Rotate(Vector3.right * -10 * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                yAxis.transform.Rotate(Vector3.right * 10 * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (!dirLock)
+                    dirLock = true;
+            }
+        }
+    }
+
+    void Kick()
+    {
+        if (dirLock)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Rb.AddForce(xAxis.transform.forward * power);
+                Rb.AddForce(yAxis.transform.forward * power);
+            }
+        }
+    }
+}
