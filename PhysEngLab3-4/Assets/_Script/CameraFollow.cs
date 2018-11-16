4ddeb88d875e0daf1db494022cdaa5538ae4b86a
Makim <jmakimramjahn@gmail.com>
2018-11-16 08:51:20 -0500

    last

---
+++ PhysEngLab3-4/Assets/_Script/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.Find("ball");
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        player = GameObject.Find("ball(clone)");
+        transform.position = player.transform.position + offset;
+    }
+}
--- PhysEngLab3-4/Assets/_Script/direction.cs
+++ PhysEngLab3-4/Assets/_Script/direction.cs
@@ -1,68 +1,139 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class direction : MonoBehaviour {
+
+    public Slider powerBar;
 
     public Rigidbody Rb;
 
     public GameObject xAxis;
     public GameObject yAxis;
+    public GameObject ball;
+    public Transform spawn;
+    public GameObject goal;
 
     public bool dirLock = false;
 
+    public bool powerUp = false;
+    public bool powerSwitch = false;
     public float power;
 
-	// Use this for initialization
-	void Start () {
+    private float timer;
 
 
+
+    // Use this for initialization
+    void Start () {
+        ball = GameObject.Find("ball");
+        dirLock = false;
+        powerUp = false;
+ 
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
         Aim();
         Kick();
-	}
+        resetBall();
+
+
+        powerBar.value = power;
+
+    }
 
     void Aim()
     {
         if (!dirLock)
         {
-            if (Input.GetKey(KeyCode.A))
-            {
-                xAxis.transform.Rotate(Vector3.up * -10 * Time.deltaTime);
+                if (Input.GetKey(KeyCode.A))
+                {
+                    xAxis.transform.Rotate(Vector3.up * -20 * Time.deltaTime);
+
+
             }
-            if (Input.GetKey(KeyCode.D))
-            {
-                xAxis.transform.Rotate(Vector3.up * 10 * Time.deltaTime);
-            }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    xAxis.transform.Rotate(Vector3.up * 20 * Time.deltaTime);
+                }
+            
 
             if (Input.GetKey(KeyCode.W))
             {
-                yAxis.transform.Rotate(Vector3.right * -10 * Time.deltaTime);
+                yAxis.transform.Rotate(Vector3.right * -20 * Time.deltaTime);
             }
             if (Input.GetKey(KeyCode.S))
             {
-                yAxis.transform.Rotate(Vector3.right * 10 * Time.deltaTime);
+                yAxis.transform.Rotate(Vector3.right * 20 * Time.deltaTime);
             }
+
+
+
+
+
+
+            if (!dirLock)
+            {
+                if (Input.GetKeyUp(KeyCode.Space))
+                {
+                    dirLock = true;
+                    powerUp = true;
+                }
+                 
+            }
+        }
+
+
+
+        if (powerUp)
+        {
             if (Input.GetKey(KeyCode.Space))
             {
-                if (!dirLock)
-                    dirLock = true;
+                if (!powerSwitch)
+                    power++;
+                if (powerSwitch)
+                    power--;
+                
+            }
+            if (Input.GetKeyUp(KeyCode.F))
+                    powerUp = false;
+        }
+
+        if (power <= 5 && power < 31)
+            powerSwitch = false;
+        if (power >= 30)
+            powerSwitch = true;
+
+    }
+    
+    void Kick()
+    {
+        if (dirLock && !powerUp)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Rb.AddForce(xAxis.transform.forward * power, ForceMode.Impulse);
+                Rb.AddForce(yAxis.transform.forward * power, ForceMode.Impulse);
             }
         }
     }
 
-    void Kick()
+    void resetBall()
     {
-        if (dirLock)
+        if (Input.GetKey(KeyCode.R))
         {
-            if (Input.GetKey(KeyCode.Space))
-            {
-                Rb.AddForce(xAxis.transform.forward * power);
-                Rb.AddForce(yAxis.transform.forward * power);
-            }
+            Instantiate(ball, spawn.position, spawn.rotation);
+            Destroy(this.gameObject);
+
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "goal")
+            goal.SetActive(true);
     }
 }
